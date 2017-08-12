8fe6c2e68cba8766b82769026021045b9852cceb
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-12 12:56:31 -0400

    Init

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <div className='row'>
+          <div className='col-xs-4'>
+            one
+          </div>
+          <div className='col-xs-4'>
+            two
+          </div>
+          <div className='col-xs-4'>
+            three
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import './stylesheets/main.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
