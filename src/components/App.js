85c451489a844ab2b5cbd36dadbe5e1698883d2d
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-12 15:29:45 -0400

    update READEME.md

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,7 @@ class App extends Component {
     return (
       <div className="App container">
         <div className='row'>
-          <div className='col-xs-4'>
-            4/12
-          </div>
-          <div className='col-xs-2'>
-            2/12
-          </div>
-          <div className='col-xs-6'>
-            6/12
-          </div>
+          
         </div>
       </div>
     );
